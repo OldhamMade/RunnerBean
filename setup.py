1653a634075fc1d2c35ba8db9a16b34ec0c3c955
Phillip Oldham <phillip.oldham@gmail.com>
2013-04-09 10:51:50 +0100

    fixes for the setup.py file to point to the correct home for assets

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ execfile('RunnerBean/__version__.py')
 setup(name="RunnerBean",
     description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     version=__version__,
-    url="http://github.com/unpluggd/RunnerBean",
+    url="https://github.com/wewriteapps/RunnerBean",
     packages=find_packages(exclude="specs"),
     dependency_links = [
-        "https://github.com/unpluggd/resolver/tarball/0.2.1#egg=resolver-0.2.1"
+        "https://github.com/wewriteapps/resolver/tarball/0.2.1#egg=resolver-0.2.1"
           ],
     install_requires=[
           'simplejson',
