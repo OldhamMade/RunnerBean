f9787dfa44ffcd08c2aea4b8b6d3c9dfbb7a1255
Phillip Oldham <phillip.oldham@gmail.com>
2013-09-11 16:20:05 +0100

    fixing issue with setuptools install

--- RunnerBean/__version__.py
+++ RunnerBean/__version__.py
@@ -1,3 +1,3 @@
 __author__	= 'Phillip B Oldham'
-__version__ = '0.4.2-dev'
+__version__ = '0.4.3-dev'
 __licence__	= 'MIT'
--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ execfile('RunnerBean/__version__.py')
 
 setup(name="RunnerBean",
     description='A simple tool for creating long-running Python workers listening for Beanstalk jobs.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     version=__version__,
     url="https://github.com/wewriteapps/RunnerBean",
     packages=find_packages(exclude="specs"),
